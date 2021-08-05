47d8f840b5d34cf121228eeccbc856f6b35f66ab
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-08-05 15:57:46 -0500

    format helpers

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const express = require('express');
 const routes = require('./controllers');
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+const helpers = require('./utils/helpers');
 const session = require('express-session');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
